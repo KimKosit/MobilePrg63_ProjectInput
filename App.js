bb48e8a3ff783b26993b0992a3a9889be72c3b9e
KimKosit <user_kho_kim@hotmail.com>
2020-08-28 14:42:08 +0700

    Finished

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, Button } from 'react-native';
 
 export default function App() {
   return (
+    // No.1
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.detailedContainer}>
+        <TextInput placeholder="Type something!" style={styles.textInput} />
+        <Button title="ADD" />
+      </View>
+      <Image source={require('./assets/logo.png')} style={styles.logo} />
     </View>
   );
 }
 
+// No.1
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    padding: 50,
+  },
+  detailedContainer: {
+    width: "100%",
+    flexDirection: 'column',
     justifyContent: 'center',
+    alignContent: "center",
+  },
+  textInput:{
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  logo:{
+    position: "absolute",
+    alignSelf: 'flex-end',
+    right: 10,
+    bottom: 10,
+    width: 89.3,
+    height: 74.4,
   },
 });
+
+// // No.2
+// const styles = StyleSheet.create({
+//   container:{
+//     flex: 1,
+//     padding: 50,
+//   },
+//   detailedContainer:{
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//     alignItems: "center",
+//   },
+//   textInput:{
+//     width: '80%',
+//     borderColor: 'black',
+//     borderWidth: 1,
+//     padding: 10,
+//   },
+//   logo:{
+//     position: "absolute",
+//     alignSelf: 'flex-end',
+//     right: 10,
+//     bottom: 10,
+//     width: 89.3,
+//     height: 74.4,
+//   },
+// });
